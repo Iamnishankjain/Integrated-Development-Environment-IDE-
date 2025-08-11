5e31e70023bd607bf3aea80c636562d81c2b925e
Iamnishankjain <jnishank123@gmail.com>
2025-08-11 22:17:52 +0530

    The project is complete

--- src/screens/Provider/PlayGroundProvider.jsx
+++ src/screens/Provider/PlayGroundProvider.jsx
@@ -5,32 +5,7 @@ import { v4 } from 'uuid';
 export const PlayGroundContext = createContext();
 
 //Intial Some folder
-const initialData=[
-  {
-    id:v4(),
-    title: 'DSA',
-    files: [
-      {
-      id: v4(),
-      title: 'index',
-      code: `cout<<"Hello world";`,
-      language: "cpp"
-      }
-    ]
-  },
-  {
-    id:v4(),
-    title: 'Frontend',
-    files: [
-      {
-      id: v4(),
-      title: 'test',
-      code: `console.log("Hello world");`,
-      language: "javascript",
-      }
-    ]
-  },
-];
+const initialData=[];
 
 const PlayGroundProvider = ({children}) => {
   const [folders,setFolders] = useState(()=>{
